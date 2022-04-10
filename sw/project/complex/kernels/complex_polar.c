3ddb9559d984daf293d9fa2561737aa72dda914f
Fe1icien <fndozi@student.ethz.ch>
2022-04-10 14:27:58 +0200

    first half of angle calculation

--- sw/project/complex/kernels/complex_polar.c
+++ sw/project/complex/kernels/complex_polar.c
@@ -76,13 +76,76 @@ void polar_ssr(double *a, double *c, uint32_t size){
 
 	
 	for(uint32_t  i = 0; i < size; i += 2){		//angle
-		register double d0;
+		register double c0 = c[i];
+		register double d0 = 1;
+		register double d1 = 1.6;
+		register double d2 = 3.6;
+		register double d3 = 7.3;
+		register double d4 = 23;
+
+		register double d5 = 0.07765;
+		register double d6 = 0.2874;
+		register double d7 = 0.9953;
+		
+		register double d8 = 0.954;
+		register double d9 = 0.337;
+		register double d10 = 0.23;
+		register double d11 = 1.4;
+
+		register double d12 = 1.2036;
+		register double d13 = 0.1288;
+		register double d14 = 2.6;
+		register double d15 = 2.73;
+		register double d16 = 0.045;
+
 		asm volatile(
-            "fdiv.d %[d0], ft0, ft1 \n"
-			:	[ d0 ] "+f"(d0)
+            "fdiv.d ft3, ft0, ft1 \n"
+			"bge ft3, x0, 4 \n"
+			"fsub.d ft3, x0, ft3 \n"
+			"fadd.d ft4, ft3, x0 \n"
+			"fmul.d ft5, ft3, ft4 \n"  //x^2
+			"fmul.d ft6, ft3, ft5 \n"	//x^3
+			"fmul.d ft7, ft5, ft6 \n"	//x^5
+			"blt %[d0], ft3, 24 \n"
+			"fmul.d ft8, %[d5], ft7 \n"	
+			"fmul.d ft9, %[d6], ft6 \n"	
+			"fmul.d ft10, %[d7], ft4 \n"
+			"fsub.d ft11, ft8, ft9 \n"
+			"fadd.d ft15, ft10, ft11 \n"	//ft15 = result
+			"jal x0, ??"					//go to end
+			"blt %[d1], ft3, 36 \n"	
+			"fadd.d ft8, %[d8], x0 \n"		//0.
+			"fsub.d ft9, ft3, %[d11] \n"
+			"fmul.d ft10, %[d9], ft9 \n"  	//1.
+			"fadd.d ft11, ft9, x0 \n"
+			"fmul.d ft12, ft11, ft9 \n"	
+			"fmul.d ft13, %[d10], ft12 \n"	//2.
+			"fadd.d ft14, ft8, ft10 \n"
+			"fsub.d ft15, ft14, ft13 \n"	//ft15 = result
+			"jal x0, ??"
+
+
+
+			
+			"blt %[d2], ft3, 36 \n"	
+			"fadd.d ft8, %[d8], x0 \n"		//0.
+			"fsub.d ft9, ft3, %[d11] \n"
+			"fmul.d ft10, %[d9], ft9 \n"  	//1.
+			"fadd.d ft11, ft9, x0 \n"
+			"fmul.d ft12, ft11, ft9 \n"	
+			"fmul.d ft13, %[d10], ft12 \n"	//2.
+			"fadd.d ft14, ft8, ft10 \n"
+			"fsub.d ft15, ft14, ft13 \n"	//ft15 = result
+			"jal x0, ??"
+
+
+
+
+
+		
+			:	[ c0 ] "+f"(c0)
 			:
-			:	"ft0","ft1");
-		if(d0 )
+			:	"ft0","ft1","ft3");
 
 		c[i] = c0;
 	}
