bb69763c8a42e39c7b0835cb7cd7412cda01d52d
Fe1icien <fndozi@student.ethz.ch>
2022-04-12 22:59:13 +0200

    sqrt lut done

---
+++ sw/project/complex/kernels/sqrt_lookuptable_gen.py
@@ -0,0 +1,46 @@
+import math
+import pathlib
+
+# 0-1  0.02
+# 1-10  0.1
+# 10-.. 1
+
+file_path = pathlib.Path('C:\\Users\\ndozi\\bachelor\\snitch\\sw\\project\\complex\\kernels')
+file = file_path / 'sqrt_lookuptable.h'
+
+
+lut_str = ''
+lut_str += '\n\n'
+lut_str += '//size = \n\n'
+lut_str += 'static const double lookup[] = { \n'
+
+i = 0
+j = 1
+
+while(i < 1):
+    lut_str += str(math.sqrt(i)) + ', '
+    if(j == 10):
+        lut_str += '\n'
+        j = 0
+    j += 1
+    i += 0.02
+while(i < 10):
+    lut_str += str(math.sqrt(i)) + ', '
+    if(j == 10):
+        lut_str += '\n'
+        j = 0
+    j += 1
+    i += 0.1
+while(i < 50):
+    lut_str += str(math.sqrt(i)) + ', '
+    if(j == 10):
+        lut_str += '\n'
+        j = 0
+    j += 1
+    i += 1
+
+lut_str += str(math.sqrt(i))
+lut_str += ' }; \n'
+
+with file.open('w') as f:
+    f.write(lut_str)
